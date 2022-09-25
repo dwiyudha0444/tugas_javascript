e7e81467f1bb39d181a3653531b6ca69a7c1e338
Dwi Yudha Saputra <112836841+dwiyudha0444@users.noreply.github.com>
2022-09-25 12:10:11 +0700

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,146 @@
+$("document").ready(function () {
+  $(".burung").click(function () {
+    $(this).css({
+      background: "url(images/burung.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/burung.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".bebek").click(function () {
+    $(this).css({
+      background: "url(images/bebek.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/bebek.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".ayam").click(function () {
+    $(this).css({
+      background: "url(images/ayam.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/ayam.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".kalkun").click(function () {
+    $(this).css({
+      background: "url(images/kalkun.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/kalkun.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".babi").click(function () {
+    $(this).css({
+      background: "url(images/babi.jpg)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/babi.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".sapi").click(function () {
+    $(this).css({
+      background: "url(images/sapi.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/sapi.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".kambing").click(function () {
+    $(this).css({
+      background: "url(images/kambing.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/kambing.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".kucing").click(function () {
+    $(this).css({
+      background: "url(images/kucing.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/kucing.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".harimau").click(function () {
+    $(this).css({
+      background: "url(images/harimau.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/harimau.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".singa").click(function () {
+    $(this).css({
+      background: "url(images/singa.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/singa.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".komodo").click(function () {
+    $(this).css({
+      background: "url(images/komodo.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/komodo.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+  $(".badak").click(function () {
+    $(this).css({
+      background: "url(images/badak.png)",
+    });
+    $("<audio></audio>")
+      .attr({
+        src: "audio/badak.mp3",
+        volume: 0.4,
+        autoplay: "autoplay",
+      })
+      .appendTo("body");
+  });
+});
